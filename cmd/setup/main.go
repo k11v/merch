308ed571e60a50fac4dae75a326392899491a1b1
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-10 16:50:21 +0300

    Add setup main impl

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/k11v/merch/internal/app"
+)
+
+func main() {
+	const envPostgresURL = "APP_POSTGRES_URL"
+	postgresURL := os.Getenv(envPostgresURL)
+	if postgresURL == "" {
+		err := fmt.Errorf("%s env is empty", envPostgresURL)
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	err := app.SetupPostgres(postgresURL)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
