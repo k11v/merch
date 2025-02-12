680a91e372f6ee2af13efb161713eabe2a484e17
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-12 13:25:22 +0300

    Add Postgres users table

--- internal/app/migrationdata/0_init.up.sql
+++ internal/app/migrationdata/0_init.up.sql
@@ -1,3 +1,14 @@
 BEGIN;
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    username text NOT NULL,
+    password_hash text NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX users_username_idx ON users (username);
+
 COMMIT;
