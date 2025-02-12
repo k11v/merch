ef46e10b481c8f6fb54ba8bad1bdacd662ba7f0c
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-13 01:17:19 +0300

    Change transfers to transactions, coin_amount to balance

--- internal/app/migrationdata/0_init.up.sql
+++ internal/app/migrationdata/0_init.up.sql
@@ -6,19 +6,19 @@ CREATE TABLE IF NOT EXISTS users (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
     username text NOT NULL,
     password_hash text NOT NULL,
-    coin_amount integer NOT NULL DEFAULT 0,
+    balance integer NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX IF NOT EXISTS users_username_idx ON users (username);
 
-CREATE TABLE IF NOT EXISTS transfers (
+CREATE TABLE IF NOT EXISTS transactions (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
-    sender_id uuid,
-    receiver_id uuid,
-    coin_amount integer NOT NULL,
+    from_user_id uuid,
+    to_user_id uuid,
+    amount integer NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (sender_id) REFERENCES users (id),
-    FOREIGN KEY (receiver_id) REFERENCES users (id)
+    FOREIGN KEY (from_user_id) REFERENCES users (id),
+    FOREIGN KEY (to_user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS items (
