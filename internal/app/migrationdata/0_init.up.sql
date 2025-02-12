ae8ad7da510c4f834a4baed75aea3fd54f76a90d
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-12 22:23:14 +0300

    Add Postgres coin and item tables

--- internal/app/migrationdata/0_init.up.sql
+++ internal/app/migrationdata/0_init.up.sql
@@ -6,9 +6,36 @@ CREATE TABLE IF NOT EXISTS users (
     id uuid NOT NULL DEFAULT uuid_generate_v4(),
     username text NOT NULL,
     password_hash text NOT NULL,
+    coin_amount integer NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX IF NOT EXISTS users_username_idx ON users (username);
 
-CREATE UNIQUE INDEX users_username_idx ON users (username);
+CREATE TABLE IF NOT EXISTS transfers (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    sender_id uuid,
+    receiver_id uuid NOT NULL,
+    coin_amount integer NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (sender_id) REFERENCES users (id),
+    FOREIGN KEY (receiver_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS items (
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    name text NOT NULL,
+    price integer NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE UNIQUE INDEX IF NOT EXISTS items_name_idx ON items (name);
+
+CREATE TABLE IF NOT EXISTS users_items (
+    user_id uuid NOT NULL,
+    item_id uuid NOT NULL,
+    amount integer NOT NULL,
+    PRIMARY KEY (user_id, item_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (item_id) REFERENCES items (id)
+);
 
 COMMIT;
