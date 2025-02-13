4698a38aeb8f3ae80c106bc3d91e53aa04cbb9b9
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-13 15:06:24 +0300

    Add formatAndSignToken and parseAndValidateToken

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/ed25519"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,7 +12,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"github.com/jackc/pgerrcode"
 	"github.com/jackc/pgx/v5"
@@ -172,7 +175,7 @@ func (h *Handler) GetAPIBuyItem(ctx context.Context, request merch.GetAPIBuyItem
 		return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
 	}
 
-	user, err = updateUserBalance(ctx, tx, userID, balance)
+	_, err = updateUserBalance(ctx, tx, userID, balance)
 	if err != nil {
 		return nil, err
 	}
@@ -889,4 +892,78 @@ func Authenticator() func(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+type Token struct {
+	UserID    uuid.UUID
+	ExpiresAt time.Time
+	IssuedAt  time.Time
+	ID        uuid.UUID
+}
+
+func NewToken(userID uuid.UUID, expiresAt time.Time) *Token {
+	return &Token{
+		UserID:    userID,
+		ExpiresAt: expiresAt,
+		IssuedAt:  time.Now(),
+		ID:        uuid.New(),
+	}
+}
+
+func formatAndSignToken(token *Token, jwtSignatureKey ed25519.PrivateKey) (string, error) {
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodEdDSA, jwt.RegisteredClaims{
+		Subject:   token.UserID.String(),
+		ExpiresAt: jwt.NewNumericDate(token.ExpiresAt), // TODO: Consider time zones.
+		IssuedAt:  jwt.NewNumericDate(token.IssuedAt),  // TODO: Consider time zones.
+		ID:        token.ID.String(),
+	})
+	return jwtToken.SignedString(jwtSignatureKey)
+}
+
+func parseAndValidateToken(s string, jwtVerificationKey ed25519.PublicKey) (*Token, error) {
+	jwtToken, err := jwt.ParseWithClaims(
+		s,
+		&jwt.RegisteredClaims{},
+		func(t *jwt.Token) (any, error) {
+			return jwtVerificationKey, nil
+		},
+		jwt.WithValidMethods([]string{jwt.SigningMethodEdDSA.Alg()}),
+	)
+	if err != nil {
+		return nil, err
+	}
+	claims := jwtToken.Claims.(*jwt.RegisteredClaims)
+
+	if claims.Subject == "" {
+		return nil, errors.New("empty sub token claim")
+	}
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return nil, fmt.Errorf("sub token claim: %w", err)
+	}
+
+	if claims.ExpiresAt == nil {
+		return nil, errors.New("empty exp token claim")
+	}
+	expiresAt := claims.ExpiresAt.Time
+
+	if claims.IssuedAt == nil {
+		return nil, errors.New("empty iat token claim")
+	}
+	issuedAt := claims.IssuedAt.Time
+
+	if claims.ID == "" {
+		return nil, errors.New("empty jti token claim")
+	}
+	id, err := uuid.Parse(claims.ID)
+	if err != nil {
+		return nil, fmt.Errorf("jti token claim: %w", err)
+	}
+
+	return &Token{
+		UserID:    userID,
+		ExpiresAt: expiresAt,
+		IssuedAt:  issuedAt,
+		ID:        id,
+	}, nil
 }
