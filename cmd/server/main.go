2a8baafc1fce5844986f7643a9936923144adef7
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-13 14:44:11 +0300

    Add GetAPIBuyItem impl

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -129,6 +129,69 @@ func NewHandler(db *pgxpool.Pool) *Handler {
 
 // GetAPIBuyItem implements merch.StrictServerInterface.
 func (h *Handler) GetAPIBuyItem(ctx context.Context, request merch.GetAPIBuyItemRequestObject) (merch.GetAPIBuyItemResponseObject, error) {
+	userID, ok := ctx.Value(ContextValueUserID).(uuid.UUID)
+	if !ok {
+		panic(fmt.Errorf("can't get %s context value", ContextValueUserID))
+	}
+
+	itemName := request.Item
+	if itemName == "" {
+		errors := "empty item"
+		return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
+	}
+
+	item, err := getItemByName(ctx, h.db, itemName)
+	if err != nil {
+		if errors.Is(err, ErrItemNotExist) {
+			errors := "item does not exist"
+			return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
+		}
+		return nil, err
+	}
+
+	tx, err := h.db.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err = tx.Rollback(ctx)
+		if err != nil && !errors.Is(err, pgx.ErrTxClosed) {
+			slog.Error("didn't rollback", "err", err)
+		}
+	}()
+
+	user, err := getUserForUpdate(ctx, tx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	balance := user.Balance
+	balance -= item.Price
+	if balance < 0 {
+		errors := "not enough coins"
+		return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
+	}
+
+	user, err = updateUserBalance(ctx, tx, userID, balance)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = createTransaction(ctx, tx, &userID, nil, item.Price)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = addUserItemAmount(ctx, tx, userID, item.ID, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return nil, err
+	}
+
 	return merch.GetAPIBuyItem200Response{}, nil
 }
 
@@ -329,7 +392,6 @@ func (h *Handler) PostAPISendCoin(ctx context.Context, request merch.PostAPISend
 	fromUserBalance := fromUser.Balance
 	fromUserBalance -= amount
 	if fromUserBalance < 0 {
-		fmt.Println(fromUser)
 		errors := "not enough coins"
 		return merch.PostAPISendCoin400JSONResponse{Errors: &errors}, nil
 	}
@@ -369,6 +431,7 @@ type pgxExecutor interface {
 }
 
 var (
+	ErrItemNotExist = errors.New("item does not exist")
 	ErrUserExist    = errors.New("user already exists")
 	ErrUserNotExist = errors.New("user does not exist")
 )
@@ -490,6 +553,25 @@ type UserItem struct {
 	Amount   int
 }
 
+func rowToUserItem(collectable pgx.CollectableRow) (*UserItem, error) {
+	type row struct {
+		UserID uuid.UUID `db:"user_id"`
+		ItemID uuid.UUID `db:"item_id"`
+		Amount int       `db:"amount"`
+	}
+
+	collected, err := pgx.RowToStructByName[row](collectable)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserItem{
+		UserID: collected.UserID,
+		ItemID: collected.ItemID,
+		Amount: collected.Amount,
+	}, nil
+}
+
 func rowToUserItemWithName(collectable pgx.CollectableRow) (*UserItem, error) {
 	type row struct {
 		UserID uuid.UUID `db:"user_id"`
@@ -510,6 +592,48 @@ func rowToUserItemWithName(collectable pgx.CollectableRow) (*UserItem, error) {
 		Amount:   collected.Amount,
 		ItemName: collected.ItemName,
 	}, nil
+}
+
+func getItemByName(ctx context.Context, db pgxExecutor, name string) (*Item, error) {
+	query := `
+		SELECT id, name, price
+		FROM items
+		WHERE name = $1
+	`
+	args := []any{name}
+
+	rows, _ := db.Query(ctx, query, args...)
+	item, err := pgx.CollectExactlyOneRow(rows, rowToItem)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrItemNotExist
+		}
+		return nil, err
+	}
+
+	return item, nil
+}
+
+func addUserItemAmount(ctx context.Context, db pgxExecutor, userID, itemID uuid.UUID, amount int) (*UserItem, error) {
+	query := `
+		INSERT INTO users_items (user_id, item_id, amount)
+		VALUES ($1, $2, $3)
+		ON CONFLICT ON CONSTRAINT users_user_id_item_id_pkey
+		DO UPDATE SET amount = users_items.amount + excluded.amount
+		RETURNING user_id, item_id, amount
+	`
+	args := []any{userID, itemID, amount}
+
+	rows, _ := db.Query(ctx, query, args...)
+	userItem, err := pgx.CollectExactlyOneRow(rows, rowToUserItem)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrUserNotExist
+		}
+		return nil, err
+	}
+
+	return userItem, nil
 }
 
 func getTransactionsByUserID(ctx context.Context, db pgxExecutor, userID uuid.UUID) ([]*Transaction, error) {
--- internal/app/migrationdata/0_init.up.sql
+++ internal/app/migrationdata/0_init.up.sql
@@ -28,12 +28,24 @@ CREATE TABLE IF NOT EXISTS items (
     PRIMARY KEY (id)
 );
 CREATE UNIQUE INDEX IF NOT EXISTS items_name_idx ON items (name);
+INSERT INTO items (id, name, price)
+VALUES ('158acfe8-95cc-4573-b3e7-4edf89823f45', 't-shirt', 80),
+       ('5317464e-36c7-4678-98af-2e2cf3138ec0', 'cup', 20),
+       ('93e4b592-77c1-4928-8e45-b0ec86cdf035', 'book', 50),
+       ('95d5a283-97be-4fb8-b3b4-0ef723877b7a', 'pen', 10),
+       ('133ce041-eac7-4f99-81ce-f72e23ec7399', 'powerbank', 200),
+       ('8f6e5480-a188-4626-a1de-87adbddbc932', 'hoody', 300),
+       ('c4273781-c5ed-4fe3-8554-eb5ee4e1ba00', 'umbrella', 200),
+       ('9b5da2b6-1024-48b4-81f7-e56ca9c711fe', 'socks', 10),
+       ('f4c77264-a433-4775-ac80-1898517ff8cd', 'wallet', 50),
+       ('4ca8f1f5-d719-42bf-8939-5e7ef51885c2', 'pink-hoody', 500)
+ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS users_items (
     user_id uuid NOT NULL,
     item_id uuid NOT NULL,
     amount integer NOT NULL,
-    PRIMARY KEY (user_id, item_id),
+    CONSTRAINT users_user_id_item_id_pkey PRIMARY KEY (user_id, item_id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (item_id) REFERENCES items (id)
 );
