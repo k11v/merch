b223f64a2244785eaa8351d7b376f54bd9f64c65
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-16 23:13:37 +0300

    Factor out app/jwtsetup.go from setup/main.go

--- cmd/setup/main.go
+++ cmd/setup/main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"crypto/ed25519"
-	"crypto/x509"
-	"encoding/pem"
-	"errors"
 	"fmt"
 	"os"
 
@@ -43,98 +38,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = SetupJWT(jwtVerificationKeyFile, jwtSignatureKeyFile)
+	err = app.SetupJWT(jwtVerificationKeyFile, jwtSignatureKeyFile)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
 	os.Exit(0)
-}
-
-func SetupJWT(pubFile, privFile string) error {
-	pubExists, err := fileExists(pubFile)
-	if err != nil {
-		return err
-	}
-	privExists, err := fileExists(privFile)
-	if err != nil {
-		return err
-	}
-	if pubExists || privExists {
-		if pubExists && privExists {
-			return nil
-		}
-		return errors.New("only one of public and private key files exists")
-	}
-
-	pub, priv, err := ed25519.GenerateKey(nil)
-	if err != nil {
-		return err
-	}
-
-	pubPEM, err := pemEncodeED25519PublicKey(pub)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(pubFile, pubPEM, 0o600)
-	if err != nil {
-		return err
-	}
-
-	privPEM, err := pemEncodeED25519PrivateKey(priv)
-	if err != nil {
-		return err
-	}
-	err = os.WriteFile(privFile, privPEM, 0o600)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func pemEncodeED25519PrivateKey(priv ed25519.PrivateKey) ([]byte, error) {
-	privX509, err := x509.MarshalPKCS8PrivateKey(priv)
-	if err != nil {
-		return nil, err
-	}
-	privPEM := &pem.Block{
-		Type:  "PRIVATE KEY",
-		Bytes: privX509,
-	}
-	privPEMBuf := new(bytes.Buffer)
-	err = pem.Encode(privPEMBuf, privPEM)
-	if err != nil {
-		return nil, err
-	}
-	return privPEMBuf.Bytes(), nil
-}
-
-func pemEncodeED25519PublicKey(pub ed25519.PublicKey) ([]byte, error) {
-	pubX509, err := x509.MarshalPKIXPublicKey(pub)
-	if err != nil {
-		return nil, err
-	}
-	pubPEM := &pem.Block{
-		Type:  "PUBLIC KEY",
-		Bytes: pubX509,
-	}
-	pubPEMBuf := new(bytes.Buffer)
-	err = pem.Encode(pubPEMBuf, pubPEM)
-	if err != nil {
-		return nil, err
-	}
-	return pubPEMBuf.Bytes(), nil
-}
-
-func fileExists(name string) (bool, error) {
-	switch _, err := os.Stat(name); {
-	case err == nil:
-		return true, nil
-	case errors.Is(err, os.ErrNotExist):
-		return false, nil
-	default:
-		return false, err
-	}
 }
---
+++ internal/app/jwtsetup.go
@@ -0,0 +1,97 @@
+package app
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"os"
+)
+
+func SetupJWT(pubFile, privFile string) error {
+	pubExists, err := fileExists(pubFile)
+	if err != nil {
+		return err
+	}
+	privExists, err := fileExists(privFile)
+	if err != nil {
+		return err
+	}
+	if pubExists || privExists {
+		if pubExists && privExists {
+			return nil
+		}
+		return errors.New("only one of public and private key files exists")
+	}
+
+	pub, priv, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return err
+	}
+
+	pubPEM, err := pemEncodeED25519PublicKey(pub)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(pubFile, pubPEM, 0o600)
+	if err != nil {
+		return err
+	}
+
+	privPEM, err := pemEncodeED25519PrivateKey(priv)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(privFile, privPEM, 0o600)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func pemEncodeED25519PrivateKey(priv ed25519.PrivateKey) ([]byte, error) {
+	privX509, err := x509.MarshalPKCS8PrivateKey(priv)
+	if err != nil {
+		return nil, err
+	}
+	privPEM := &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: privX509,
+	}
+	privPEMBuf := new(bytes.Buffer)
+	err = pem.Encode(privPEMBuf, privPEM)
+	if err != nil {
+		return nil, err
+	}
+	return privPEMBuf.Bytes(), nil
+}
+
+func pemEncodeED25519PublicKey(pub ed25519.PublicKey) ([]byte, error) {
+	pubX509, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		return nil, err
+	}
+	pubPEM := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubX509,
+	}
+	pubPEMBuf := new(bytes.Buffer)
+	err = pem.Encode(pubPEMBuf, pubPEM)
+	if err != nil {
+		return nil, err
+	}
+	return pubPEMBuf.Bytes(), nil
+}
+
+func fileExists(name string) (bool, error) {
+	switch _, err := os.Stat(name); {
+	case err == nil:
+		return true, nil
+	case errors.Is(err, os.ErrNotExist):
+		return false, nil
+	default:
+		return false, err
+	}
+}
