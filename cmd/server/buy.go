2c88fb521fb7fd400f773dc3c9c4b43819203d1e
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-23 19:18:26 +0300

    Extract purchase package impl

--- cmd/server/buy.go
+++ cmd/server/buy.go
@@ -4,32 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log/slog"
 
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/k11v/merch/api/merch"
+	"github.com/k11v/merch/internal/purchase"
 )
-
-var (
-	ErrItemNotExist  = errors.New("item does not exist")
-	ErrCoinNotEnough = errors.New("not enough coin")
-)
-
-type Item struct {
-	ID    uuid.UUID
-	Name  string
-	Price int
-}
-
-type UserItem struct {
-	UserID   uuid.UUID
-	ItemID   uuid.UUID
-	ItemName string
-	Amount   int
-}
 
 // GetAPIBuyItem implements merch.StrictServerInterface.
 func (h *Handler) GetAPIBuyItem(ctx context.Context, request merch.GetAPIBuyItemRequestObject) (merch.GetAPIBuyItemResponseObject, error) {
@@ -44,14 +24,14 @@ func (h *Handler) GetAPIBuyItem(ctx context.Context, request merch.GetAPIBuyItem
 		return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
 	}
 
-	purchaser := NewPurchaser(h.db)
+	purchaser := purchase.NewPurchaser(h.db)
 	err := purchaser.PurchaseByName(ctx, itemName, userID)
 	if err != nil {
-		if errors.Is(err, ErrItemNotExist) {
+		if errors.Is(err, purchase.ErrItemNotExist) {
 			errors := "item does not exist"
 			return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
 		}
-		if errors.Is(err, ErrCoinNotEnough) {
+		if errors.Is(err, purchase.ErrCoinNotEnough) {
 			errors := "not enough coin"
 			return merch.GetAPIBuyItem400JSONResponse{Errors: &errors}, nil
 		}
@@ -59,186 +39,4 @@ func (h *Handler) GetAPIBuyItem(ctx context.Context, request merch.GetAPIBuyItem
 	}
 
 	return merch.GetAPIBuyItem200Response{}, nil
-}
-
-type Purchaser struct {
-	db *pgxpool.Pool
-}
-
-func NewPurchaser(db *pgxpool.Pool) *Purchaser {
-	return &Purchaser{db: db}
-}
-
-func (h *Purchaser) PurchaseByName(ctx context.Context, itemName string, userID uuid.UUID) error {
-	item, err := getItemByName(ctx, h.db, itemName)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	tx, err := h.db.Begin(ctx)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-	defer func() {
-		err = tx.Rollback(ctx)
-		if err != nil && !errors.Is(err, pgx.ErrTxClosed) {
-			slog.Error("didn't rollback", "err", err)
-		}
-	}()
-
-	user, err := getUserForUpdate(ctx, tx, userID)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	balance := user.Balance
-	balance -= item.Price
-	if balance < 0 {
-		return fmt.Errorf("Purchaser: %w", ErrCoinNotEnough)
-	}
-
-	_, err = updateUserBalance(ctx, tx, userID, balance)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	_, err = createTransaction(ctx, tx, &userID, nil, item.Price)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	_, err = addUserItemAmount(ctx, tx, userID, item.ID, 1)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		return fmt.Errorf("Purchaser: %w", err)
-	}
-
-	return nil
-}
-
-func getItemByName(ctx context.Context, db pgxExecutor, name string) (*Item, error) {
-	query := `
-		SELECT id, name, price
-		FROM items
-		WHERE name = $1
-	`
-	args := []any{name}
-
-	rows, _ := db.Query(ctx, query, args...)
-	item, err := pgx.CollectExactlyOneRow(rows, rowToItem)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrItemNotExist
-		}
-		return nil, err
-	}
-
-	return item, nil
-}
-
-func getUserForUpdate(ctx context.Context, db pgxExecutor, id uuid.UUID) (*User, error) {
-	query := `
-		SELECT id, username, password_hash, balance
-		FROM users
-		WHERE id = $1
-		FOR UPDATE
-	`
-	args := []any{id}
-
-	rows, _ := db.Query(ctx, query, args...)
-	user, err := pgx.CollectExactlyOneRow(rows, rowToUser)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrUserNotExist
-		}
-		return nil, err
-	}
-
-	return user, nil
-}
-
-func addUserItemAmount(ctx context.Context, db pgxExecutor, userID, itemID uuid.UUID, amount int) (*UserItem, error) {
-	query := `
-		INSERT INTO users_items (user_id, item_id, amount)
-		VALUES ($1, $2, $3)
-		ON CONFLICT ON CONSTRAINT users_user_id_item_id_pkey
-		DO UPDATE SET amount = users_items.amount + excluded.amount
-		RETURNING user_id, item_id, amount
-	`
-	args := []any{userID, itemID, amount}
-
-	rows, _ := db.Query(ctx, query, args...)
-	userItem, err := pgx.CollectExactlyOneRow(rows, rowToUserItem)
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return nil, ErrUserNotExist
-		}
-		return nil, err
-	}
-
-	return userItem, nil
-}
-
-func rowToItem(collectable pgx.CollectableRow) (*Item, error) {
-	type row struct {
-		ID    uuid.UUID `db:"id"`
-		Name  string    `db:"name"`
-		Price int       `db:"price"`
-	}
-
-	collected, err := pgx.RowToStructByName[row](collectable)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Item{
-		ID:    collected.ID,
-		Name:  collected.Name,
-		Price: collected.Price,
-	}, nil
-}
-
-func rowToUserItem(collectable pgx.CollectableRow) (*UserItem, error) {
-	type row struct {
-		UserID uuid.UUID `db:"user_id"`
-		ItemID uuid.UUID `db:"item_id"`
-		Amount int       `db:"amount"`
-	}
-
-	collected, err := pgx.RowToStructByName[row](collectable)
-	if err != nil {
-		return nil, err
-	}
-
-	return &UserItem{
-		UserID: collected.UserID,
-		ItemID: collected.ItemID,
-		Amount: collected.Amount,
-	}, nil
-}
-
-func rowToUserItemWithName(collectable pgx.CollectableRow) (*UserItem, error) {
-	type row struct {
-		UserID uuid.UUID `db:"user_id"`
-		ItemID uuid.UUID `db:"item_id"`
-		Amount int       `db:"amount"`
-
-		ItemName string `db:"item_name"`
-	}
-
-	collected, err := pgx.RowToStructByName[row](collectable)
-	if err != nil {
-		return nil, err
-	}
-
-	return &UserItem{
-		UserID:   collected.UserID,
-		ItemID:   collected.ItemID,
-		Amount:   collected.Amount,
-		ItemName: collected.ItemName,
-	}, nil
 }
