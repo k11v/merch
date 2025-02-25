d6622239f91a12f755f596669b8dfd5c3309e0c2
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-25 19:27:19 +0300

    Add transfer.TestGetter

---
+++ internal/transfer/getter_test.go
@@ -0,0 +1,31 @@
+package transfer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/k11v/merch/internal/app/apptest"
+	"github.com/k11v/merch/internal/auth"
+)
+
+func TestGetter(t *testing.T) {
+	t.Run("gets transfers by user ID", func(t *testing.T) {
+		var (
+			ctx = context.Background()
+			db  = apptest.NewPostgresPool(t, ctx)
+			ph  = auth.NewPasswordHasher(auth.DefaultArgon2IDParams())
+			pa  = auth.NewPasswordAuthenticator(db, ph)
+			g   = NewGetter(db)
+		)
+
+		aliceData, err := pa.AuthenticatePassword(ctx, "alice", "alice123")
+		if err != nil {
+			t.Fatalf("got %v error", err)
+		}
+
+		_, err = g.GetTransfersByUserID(ctx, aliceData.UserID)
+		if err != nil {
+			t.Fatalf("got %v error", err)
+		}
+	})
+}
