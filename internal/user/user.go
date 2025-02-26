ad01db57c8ae1f094beb7df42581916472427233
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-26 12:13:10 +0300

    Add user.Getter.GetUser

--- internal/user/user.go
+++ internal/user/user.go
@@ -56,6 +56,14 @@ func NewGetter(db app.PgxExecutor) *Getter {
 	return &Getter{db: db}
 }
 
+func (g *Getter) GetUser(ctx context.Context, id uuid.UUID) (*User, error) {
+	u, err := getUser(ctx, g.db, id)
+	if err != nil {
+		return nil, fmt.Errorf("user.Getter: %w", err)
+	}
+	return u, nil
+}
+
 func (g *Getter) GetUserByUsername(ctx context.Context, username string) (*User, error) {
 	u, err := getUserByUsername(ctx, g.db, username)
 	if err != nil {
@@ -78,6 +86,26 @@ func createUser(ctx context.Context, db app.PgxExecutor, username string, passwo
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && isConstraintPgError(pgErr, "users_username_idx") {
 			return nil, ErrExist
+		}
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func getUser(ctx context.Context, db app.PgxExecutor, id uuid.UUID) (*User, error) {
+	query := `
+		SELECT id, username, password_hash, balance
+		FROM users
+		WHERE id = $1
+	`
+	args := []any{id}
+
+	rows, _ := db.Query(ctx, query, args...)
+	u, err := pgx.CollectExactlyOneRow(rows, rowToUser)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNotExist
 		}
 		return nil, err
 	}
