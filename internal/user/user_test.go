1a7f141a92a5894906ed11dbd137736149837551
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-26 12:13:10 +0300

    Add user.Getter.GetUser

--- internal/user/user_test.go
+++ internal/user/user_test.go
@@ -8,27 +8,54 @@ import (
 )
 
 func Test(t *testing.T) {
+	var (
+		ctx = context.Background()
+		db  = apptest.NewPostgresPool(t, ctx)
+	)
+
 	t.Run("creates and gets user with initial balance", func(t *testing.T) {
 		var (
-			ctx = context.Background()
-			tx  = apptest.NewPostgresPool(t, ctx)
-			ph  = NewPasswordHasher(DefaultArgon2IDParams())
-			c   = NewCreator(tx, ph)
-			g   = NewGetter(tx)
+			tx = apptest.BeginPostgresTx(t, ctx, db)
+			ph = NewPasswordHasher(DefaultArgon2IDParams())
+			c  = NewCreator(tx, ph)
+			g  = NewGetter(tx)
 		)
 
-		_, err := c.CreateUser(ctx, "alice", "alice123")
+		u, err := c.CreateUser(ctx, "alice", "alice123")
 		if err != nil {
 			t.Fatalf("got %v error", err)
 		}
 
-		u, err := g.GetUserByUsername(ctx, "alice")
+		u, err = g.GetUser(ctx, u.ID)
 		if err != nil {
 			t.Fatalf("got %v error", err)
 		}
 
 		if got, want := u.Balance, InitialBalance; got != want {
 			t.Fatalf("got %d balance, want %d", got, want)
+		}
+	})
+
+	t.Run("gets user by username", func(t *testing.T) {
+		var (
+			tx = apptest.BeginPostgresTx(t, ctx, db)
+			ph = NewPasswordHasher(DefaultArgon2IDParams())
+			c  = NewCreator(tx, ph)
+			g  = NewGetter(tx)
+		)
+
+		cu, err := c.CreateUser(ctx, "alice", "alice123")
+		if err != nil {
+			t.Fatalf("got %v error", err)
+		}
+
+		gu, err := g.GetUserByUsername(ctx, "alice")
+		if err != nil {
+			t.Fatalf("got %v error", err)
+		}
+
+		if got, want := cu.ID, gu.ID; got != want {
+			t.Fatalf("got %d user ID, want %d", got, want)
 		}
 	})
 }
