63077219e37f79ab319763501ce6b7721e6bc6ac
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-02-27 18:42:18 +0300

    Add user.DataCreator with CreateUsers method

--- internal/app/pgxexecutor.go
+++ internal/app/pgxexecutor.go
@@ -12,5 +12,6 @@ type PgxExecutor interface {
 	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
 	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
 	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
 }
