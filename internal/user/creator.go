fccc340429f48325ddaf1eeabfdd7545f75f0398
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-27 18:42:18 +0300

    Add user.DataCreator with CreateUsers method

--- internal/user/creator.go
+++ internal/user/creator.go
@@ -15,12 +15,13 @@ import (
 const DefaultBalance = 1000
 
 type Creator struct {
-	db app.PgxExecutor
+	dc *DataCreator
 	ph *PasswordHasher
 }
 
 func NewCreator(db app.PgxExecutor, ph *PasswordHasher) *Creator {
-	return &Creator{db: db, ph: ph}
+	dc := NewDataCreator(db)
+	return &Creator{dc: dc, ph: ph}
 }
 
 func (c *Creator) CreateUser(ctx context.Context, username string, password string) (*User, error) {
@@ -28,22 +29,42 @@ func (c *Creator) CreateUser(ctx context.Context, username string, password stri
 	if err != nil {
 		return nil, fmt.Errorf("user.Creator: %w", err)
 	}
-	u, err := createUser(ctx, c.db, username, passwordHash, DefaultBalance)
+	u, err := c.dc.CreateUser(ctx, &DataCreatorCreateUserParams{
+		Username:     username,
+		PasswordHash: passwordHash,
+		Balance:      DefaultBalance,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("user.Creator: %w", err)
 	}
 	return u, nil
 }
 
-func createUser(ctx context.Context, db app.PgxExecutor, username string, passwordHash string, balance int) (*User, error) {
+// DataCreator creates user data.
+// Usually [Creator] should be used to create users.
+type DataCreator struct {
+	db app.PgxExecutor
+}
+
+func NewDataCreator(db app.PgxExecutor) *DataCreator {
+	return &DataCreator{db: db}
+}
+
+type DataCreatorCreateUserParams struct {
+	Username     string
+	PasswordHash string
+	Balance      int
+}
+
+func (dc *DataCreator) CreateUser(ctx context.Context, params *DataCreatorCreateUserParams) (*User, error) {
 	query := `
 		INSERT INTO users (username, password_hash, balance)
 		VALUES ($1, $2, $3)
 		RETURNING id, username, password_hash, balance
 	`
-	args := []any{username, passwordHash, balance}
+	args := []any{params.Username, params.PasswordHash, params.Balance}
 
-	rows, _ := db.Query(ctx, query, args...)
+	rows, _ := dc.db.Query(ctx, query, args...)
 	u, err := pgx.CollectExactlyOneRow(rows, RowToUser)
 	if err != nil {
 		var pgErr *pgconn.PgError
@@ -54,6 +75,26 @@ func createUser(ctx context.Context, db app.PgxExecutor, username string, passwo
 	}
 
 	return u, nil
+}
+
+func (dc *DataCreator) CreateUsers(ctx context.Context, users []*DataCreatorCreateUserParams) error {
+	_, err := dc.db.CopyFrom(
+		ctx,
+		pgx.Identifier{"users"},
+		[]string{"username", "password_hash", "balance"},
+		pgx.CopyFromSlice(len(users), func(i int) ([]any, error) {
+			u := users[i]
+			return []any{u.Username, u.PasswordHash, u.Balance}, nil
+		}),
+	)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) && isConstraintPgError(pgErr, "users_username_idx") {
+			return ErrExist
+		}
+		return err
+	}
+	return nil
 }
 
 func isConstraintPgError(e *pgconn.PgError, constraint string) bool {
