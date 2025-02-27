a2e68d9113aae398e0cdd0078434652dd460634e
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-02-27 21:39:01 +0300

    Tidy

--- cmd/setuptest/main.go
+++ cmd/setuptest/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/k11v/merch/internal/app"
-	"github.com/k11v/merch/internal/user"
-	"github.com/k11v/merch/internal/user/usertest"
 )
 
 var (
@@ -49,7 +46,7 @@ func run(postgresURL string) error {
 
 	var users []*User
 	if *usersFlag > 0 {
-		users, err = createUsers(ctx, db, *usersFlag)
+		users, err = CreateUsers(ctx, db, *usersFlag)
 		if err != nil {
 			return err
 		}
@@ -57,71 +54,11 @@ func run(postgresURL string) error {
 	}
 
 	if *writeUsersFlag != "" {
-		err = writeUsersFile(*writeUsersFlag, users)
+		err = WriteUsersFile(*writeUsersFlag, users)
 		if err != nil {
 			return err
 		}
 		slog.Info("written users file", "name", *writeUsersFlag)
-	}
-
-	return nil
-}
-
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-func createUsers(ctx context.Context, db app.PgxExecutor, count int) ([]*User, error) {
-	password := usertest.DefaultPassword
-
-	users := make([]*User, count)
-	for i := range len(users) {
-		users[i] = &User{
-			Username: fmt.Sprintf("u%d", i),
-			Password: password,
-		}
-	}
-
-	passwordHasher := user.NewPasswordHasher(user.DefaultArgon2IDParams())
-	passwordHash, err := passwordHasher.Hash(password)
-	if err != nil {
-		return nil, err
-	}
-
-	createUserParamsUsers := make([]*user.DataCreatorCreateUserParams, count)
-	for i, u := range users {
-		createUserParamsUsers[i] = &user.DataCreatorCreateUserParams{
-			Username:     u.Username,
-			PasswordHash: passwordHash,
-			Balance:      user.DefaultBalance,
-		}
-	}
-
-	dataCreator := user.NewDataCreator(db)
-	err = dataCreator.CreateUsers(ctx, createUserParamsUsers)
-	if err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
-func writeUsersFile(name string, users []*User) error {
-	f, err := os.Create(name)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		closeErr := f.Close()
-		if closeErr != nil {
-			slog.Error("didn't close file", "err", err)
-		}
-	}()
-
-	err = json.NewEncoder(f).Encode(users)
-	if err != nil {
-		return err
 	}
 
 	return nil
