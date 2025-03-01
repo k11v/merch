328e8e583ef2aae0f4152f752e052ca0295d63fa
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-01 23:26:54 +0300

    Change setuptest to use envs instead of flags

--- cmd/setuptest/main.go
+++ cmd/setuptest/main.go
@@ -2,24 +2,15 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log/slog"
 	"os"
+	"strconv"
 
 	"github.com/k11v/merch/internal/app"
 )
 
-var (
-	genUsersFlag   = flag.Int("genusers", 0, "number of users to generate")
-	writeUsersFlag = flag.String("writeusers", "", "write users to JSON file")
-	genAuthsFlag   = flag.Bool("genauths", false, "whether to generate auth tokens for users")
-	writeAuthsFlag = flag.String("writeauths", "", "write auth tokens to JSON file")
-)
-
 func main() {
-	flag.Parse()
-
 	const envPostgresURL = "APP_POSTGRES_URL"
 	postgresURL := os.Getenv(envPostgresURL)
 	if postgresURL == "" {
@@ -36,7 +27,60 @@ func main() {
 		os.Exit(1)
 	}
 
-	err := run(postgresURL, jwtSignatureKeyFile)
+	const envUserFile = "APPTEST_USER_FILE"
+	userFile := os.Getenv(envUserFile)
+
+	const envUserGenerate = "APPTEST_USER_GENERATE"
+	userGenerateEnv := os.Getenv(envUserGenerate)
+	userGenerate := false
+	if userGenerateEnv != "" {
+		var err error
+		userGenerate, err = strconv.ParseBool(userGenerateEnv)
+		if err != nil {
+			err = fmt.Errorf("%s env: %w", envUserGenerate, err)
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	const envUserGenerateCount = "APPTEST_USER_GENERATE_COUNT"
+	userGenerateCountEnv := os.Getenv(envUserGenerateCount)
+	userGenerateCount := 0
+	if userGenerateCountEnv != "" {
+		var err error
+		userGenerateCount, err = strconv.Atoi(userGenerateCountEnv)
+		if err != nil {
+			err = fmt.Errorf("%s env: %w", envUserGenerateCount, err)
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	const envAuthFile = "APPTEST_AUTH_FILE"
+	authFile := os.Getenv(envAuthFile)
+
+	const envAuthGenerate = "APPTEST_AUTH_GENERATE"
+	authGenerateEnv := os.Getenv(envAuthGenerate)
+	authGenerate := false
+	if authGenerateEnv != "" {
+		var err error
+		authGenerate, err = strconv.ParseBool(authGenerateEnv)
+		if err != nil {
+			err = fmt.Errorf("%s env: %w", envAuthGenerate, err)
+			_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	err := run(&runParams{
+		PostgresURL:         postgresURL,
+		JWTSignatureKeyFile: jwtSignatureKeyFile,
+		UserFile:            userFile,
+		UserGenerate:        userGenerate,
+		UserGenerateCount:   userGenerateCount,
+		AuthFile:            authFile,
+		AuthGenerate:        authGenerate,
+	})
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
@@ -45,41 +89,49 @@ func main() {
 	os.Exit(0)
 }
 
-func run(postgresURL string, jwtSignatureKeyFile string) error {
+type runParams struct {
+	PostgresURL         string
+	JWTSignatureKeyFile string
+	UserFile            string
+	UserGenerate        bool
+	UserGenerateCount   int
+	AuthFile            string
+	AuthGenerate        bool
+}
+
+func run(params *runParams) error {
 	ctx := context.Background()
 
-	db, err := app.NewPostgresPool(ctx, postgresURL)
+	db, err := app.NewPostgresPool(ctx, params.PostgresURL)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	jwtSignatureKey, err := app.ReadFileED25519PrivateKey(jwtSignatureKeyFile)
+	jwtSignatureKey, err := app.ReadFileED25519PrivateKey(params.JWTSignatureKeyFile)
 	if err != nil {
 		return err
 	}
 
 	var users map[string]*User
-	userCount := *genUsersFlag
-	if userCount > 0 {
-		users, err = GenerateUsers(ctx, db, userCount)
+	if params.UserGenerate {
+		users, err = GenerateUsers(ctx, db, params.UserGenerateCount)
 		if err != nil {
 			return err
 		}
 		slog.Info("generated users", "count", len(users))
 	}
 
-	userFile := *writeUsersFlag
-	if userFile != "" {
-		err = WriteFileJSON(userFile, users)
+	if params.UserFile != "" {
+		err = WriteFileJSON(params.UserFile, users)
 		if err != nil {
 			return err
 		}
-		slog.Info("written user file", "name", userFile)
+		slog.Info("written user file", "name", params.UserFile)
 	}
 
 	var authTokens map[string]string
-	if *genAuthsFlag {
+	if params.AuthGenerate {
 		authTokens, err = GenerateAuthTokens(ctx, db, jwtSignatureKey, users)
 		if err != nil {
 			return err
@@ -87,13 +139,12 @@ func run(postgresURL string, jwtSignatureKeyFile string) error {
 		slog.Info("generated auth tokens", "count", len(authTokens))
 	}
 
-	authTokenFile := *writeAuthsFlag
-	if authTokenFile != "" {
-		err = WriteFileJSON(authTokenFile, authTokens)
+	if params.AuthFile != "" {
+		err = WriteFileJSON(params.AuthFile, authTokens)
 		if err != nil {
 			return err
 		}
-		slog.Info("written auth token file", "name", authTokenFile)
+		slog.Info("written auth file", "name", params.AuthFile)
 	}
 
 	return nil
