0681adb6cf26b8e858add8fb4290a88856df564a
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2025-03-01 22:44:36 +0300

    Change setuptest GenerateUsers to use new CreateUsers

--- cmd/setuptest/user.go
+++ cmd/setuptest/user.go
@@ -7,27 +7,22 @@ import (
 	"log/slog"
 	"os"
 
+	"github.com/google/uuid"
+
 	"github.com/k11v/merch/internal/app"
 	"github.com/k11v/merch/internal/user"
 	"github.com/k11v/merch/internal/user/usertest"
 )
 
 type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Balance  int       `json:"balance"`
 }
 
 func GenerateUsers(ctx context.Context, db app.PgxExecutor, count int) ([]*User, error) {
 	password := usertest.DefaultPassword
-
-	users := make([]*User, count)
-	for i := range len(users) {
-		users[i] = &User{
-			Username: fmt.Sprintf("u%d", i),
-			Password: password,
-		}
-	}
-
 	passwordHasher := user.NewPasswordHasher(user.DefaultArgon2IDParams())
 	passwordHash, err := passwordHasher.Hash(password)
 	if err != nil {
@@ -35,18 +30,28 @@ func GenerateUsers(ctx context.Context, db app.PgxExecutor, count int) ([]*User,
 	}
 
 	createUserParamsUsers := make([]*user.DataCreatorCreateUserParams, count)
-	for i, u := range users {
+	for i := range len(createUserParamsUsers) {
 		createUserParamsUsers[i] = &user.DataCreatorCreateUserParams{
-			Username:     u.Username,
+			Username:     fmt.Sprintf("u%d", i),
 			PasswordHash: passwordHash,
 			Balance:      user.DefaultBalance,
 		}
 	}
 
 	dataCreator := user.NewDataCreator(db)
-	err = dataCreator.CreateUsers(ctx, createUserParamsUsers)
+	userUsers, err := dataCreator.CreateUsers(ctx, createUserParamsUsers)
 	if err != nil {
 		return nil, fmt.Errorf("GenerateUsers: %w", err)
+	}
+
+	users := make([]*User, len(userUsers))
+	for i, u := range userUsers {
+		users[i] = &User{
+			ID:       u.ID,
+			Username: u.Username,
+			Password: password,
+			Balance:  u.Balance,
+		}
 	}
 
 	return users, nil
